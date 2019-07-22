462f4d7f723cd48d615f3e2c9adf1e74658deffa
Tayfun CESUR <tayfuncesur35@gmail.com>
2019-07-22 20:06:43 +0300

    Sample project removed from gitignore

---
+++ sample/src/main/java/com/tayfuncesur/sample/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.tayfuncesur.sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.karacce.beetle.Beetle
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var beetle: Beetle
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        beetle =(application as App).beetle
+        beetle.setActivity(this)
+        beetle.onShake()
+    }
+}
