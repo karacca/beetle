48fcef8a9a76b4da191951771b8020b2f80b59a7
Omer Karaca <omerkaracca@gmail.com>
2022-07-18 23:22:20 +0300

    Add input validation to ReportActivity

--- beetle/src/main/kotlin/com/karacca/beetle/ui/ReportActivity.kt
+++ beetle/src/main/kotlin/com/karacca/beetle/ui/ReportActivity.kt
@@ -4,11 +4,15 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.activity.result.contract.ActivityResultContract
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatImageView
+import androidx.core.view.get
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.card.MaterialCardView
+import com.google.android.material.textfield.TextInputEditText
 import com.karacca.beetle.R
 
 /**
@@ -16,7 +20,7 @@ import com.karacca.beetle.R
  * @date 13.07.2022
  */
 
-internal class ReportActivity : AppCompatActivity() {
+internal class ReportActivity : AppCompatActivity(), TextWatcher {
 
     private lateinit var screenshot: Uri
 
@@ -24,6 +28,8 @@ internal class ReportActivity : AppCompatActivity() {
     private lateinit var imageView: AppCompatImageView
     private lateinit var screenshotCardView: MaterialCardView
     private lateinit var logsCardView: MaterialCardView
+    private lateinit var titleEditText: TextInputEditText
+    private lateinit var descriptionEditText: TextInputEditText
 
     private val openEditActivity = registerForActivityResult(
         object : ActivityResultContract<Uri, Uri>() {
@@ -53,15 +59,30 @@ internal class ReportActivity : AppCompatActivity() {
         imageView = findViewById(R.id.image_view_screenshot)
         screenshotCardView = findViewById(R.id.card_view_screenshot)
         logsCardView = findViewById(R.id.card_view_logs)
+        titleEditText = findViewById(R.id.edit_text_title)
+        descriptionEditText = findViewById(R.id.edit_text_description)
 
         toolbar.setNavigationOnClickListener { finish() }
         imageView.setImageURI(screenshot)
         screenshotCardView.setOnClickListener {
             openEditActivity.launch(screenshot)
         }
+
+        titleEditText.addTextChangedListener(this)
+        descriptionEditText.addTextChangedListener(this)
     }
 
     companion object {
         const val ARG_SCREENSHOT = "screenshot"
+    }
+
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+    override fun afterTextChanged(p0: Editable?) {
+        val isTitleMissing = titleEditText.text.isNullOrEmpty()
+        val isDescriptionMissing = descriptionEditText.text.isNullOrEmpty()
+        toolbar.menu[0].isEnabled = !isTitleMissing && !isDescriptionMissing
     }
 }
